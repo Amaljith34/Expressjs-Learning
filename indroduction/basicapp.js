24f47eec6c3fc6e12a8fb092a8444615540b15a1
Amaljith-aj <jith51541@gmail.com>
2024-09-09 15:27:31 +0530

    create small app using http method

---
+++ indroduction/basicapp.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const app=express();
+const PORT=3000;
+
+app.get('/',(req,res)=>{
+      res.send('hello world this is express')
+})
+
+
+//productpage
+app.get('/product',(req,res)=>{
+    res.send('this is product page');
+})
+
+///aboutpage
+app.get('/about',(req,res)=>{
+    res.send('this is about page');
+})
+
+// contact
+app.get('/contact',(req,res)=>{
+    res.send('this is contact page');
+})
+
+
+app.listen(PORT,()=>{
+    console.log('running');
+})
