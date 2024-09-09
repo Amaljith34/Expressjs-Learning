acbe2016181f93bd6d9d0ad83e2c6f0531fcc0ea
Amaljith-aj <jith51541@gmail.com>
2024-09-09 16:44:19 +0530

    static file

--- indroduction/basicapp.js
+++ indroduction/basicapp.js
@@ -5,24 +5,25 @@ const PORT=3000;
 
 
 
-app.get('/',(req,res)=>{
-      res.send('hello world this is express')
-})
 
-productpage
-app.get('/product',(req,res)=>{
-    res.send('this is product page');
-})
+// app.get('/',(req,res)=>{
+//       res.send('hello world this is express')
+// })
 
-///aboutpage
-app.get('/about',(req,res)=>{
-    res.send('this is about page');
-})
+//productpage
+// app.get('/product',(req,res)=>{
+//     res.send('this is product page');
+// })
 
-// contact
-app.get('/contact',(req,res)=>{
-    res.send('this is contact page');
-})
+// ///aboutpage
+// app.get('/about',(req,res)=>{
+//     res.send('this is about page');
+// })
+
+// // contact
+// app.get('/contact',(req,res)=>{
+//     res.send('this is contact page');
+// })
 
 
 
@@ -35,21 +36,19 @@ app.get('/contact',(req,res)=>{
 ////////methods
 
 
+// app.post('/product',(req,res)=>{
+//     res.send('this is product page');
+// })
 
-app.post('/product',(req,res)=>{
-    res.send('this is product page');
-})
+// ///aboutpage
+// app.put('/about',(req,res)=>{
+//     res.send('this is about page');
+// })
 
-///aboutpage
-app.put('/about',(req,res)=>{
-    res.send('this is about page');
-})
-
-// contact
-app.delete('/contact',(req,res)=>{
-    res.send('this is contact page');
-})
-
+// // contact
+// app.delete('/contact',(req,res)=>{
+//     res.send('this is contact page');
+// })
 
 
 
@@ -57,22 +56,29 @@ app.delete('/contact',(req,res)=>{
 
 
 
-///file
 
-app.get('/',(req,res)=>{
-    // res.sendFile('./index.html')
-    res.sendFile(path.join(__dirname,'index.html'))
-})
+// ///file
 
-app.get('/about',(req,res)=>{
-    res.sendFile(path.join(__dirname,'about.html'))
-})
-app.get('/contact',(req,res)=>{
-    res.sendFile(path.join(__dirname,'contact.html'))
-})
-app.get('/product',(req,res)=>{
-    res.sendFile(path.join(__dirname,'product.html'))
-})
+// app.get('/',(req,res)=>{
+//     // res.sendFile('./index.html')
+//     res.sendFile(path.join(__dirname,'index.html'))
+// })
+
+// app.get('/about',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'about.html'))
+// })
+// app.get('/contact',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'contact.html'))
+// })
+// app.get('/product',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'product.html'))
+// })
+
+
+
+//static file
+app.use(express.static('public'))
+
 
 
 
