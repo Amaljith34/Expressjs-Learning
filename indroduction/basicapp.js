1cd01b4c42ad822214b28999cd55b7a65d9f2a27
Amaljith-aj <jith51541@gmail.com>
2024-09-09 16:30:57 +0530

    route files

--- indroduction/basicapp.js
+++ indroduction/basicapp.js
@@ -1,34 +1,40 @@
 const express=require('express');
 const app=express();
+const path=require('path')
 const PORT=3000;
+
+
 
 app.get('/',(req,res)=>{
       res.send('hello world this is express')
 })
 
+productpage
+app.get('/product',(req,res)=>{
+    res.send('this is product page');
+})
 
-//productpage
-// app.get('/product',(req,res)=>{
-//     res.send('this is product page');
-// })
+///aboutpage
+app.get('/about',(req,res)=>{
+    res.send('this is about page');
+})
 
-// ///aboutpage
-// app.get('/about',(req,res)=>{
-//     res.send('this is about page');
-// })
-
-// // contact
-// app.get('/contact',(req,res)=>{
-//     res.send('this is contact page');
-// })
+// contact
+app.get('/contact',(req,res)=>{
+    res.send('this is contact page');
+})
 
 
-// app.listen(PORT,()=>{
-//     console.log('running');
-// })
+
+
+
+
+
 
 
 ////////methods
+
+
 
 app.post('/product',(req,res)=>{
     res.send('this is product page');
@@ -43,6 +49,37 @@ app.put('/about',(req,res)=>{
 app.delete('/contact',(req,res)=>{
     res.send('this is contact page');
 })
+
+
+
+
+
+
+
+
+///file
+
+app.get('/',(req,res)=>{
+    // res.sendFile('./index.html')
+    res.sendFile(path.join(__dirname,'index.html'))
+})
+
+app.get('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname,'about.html'))
+})
+app.get('/contact',(req,res)=>{
+    res.sendFile(path.join(__dirname,'contact.html'))
+})
+app.get('/product',(req,res)=>{
+    res.sendFile(path.join(__dirname,'product.html'))
+})
+
+
+
+
+
+
+
 
 
 app.listen(PORT,()=>{
