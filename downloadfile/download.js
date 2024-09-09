e261ccbef5ad0d8decb820e86c978377a5c1d958
Amaljith-aj <jith51541@gmail.com>
2024-09-09 18:09:12 +0530

    downloadfile

--- downloadfile/download.js
+++ downloadfile/download.js
@@ -4,10 +4,22 @@ const PORT=3030;
 const path=require('path')
 
 
+// app.get('/',(req,res)=>{
+//     // res.send('hello world')
+//     res.download(path.join(__dirname,'sample.txt'))
+// })
+app.use(express.static(path.join(__dirname,'public')))
+
+
 app.get('/',(req,res)=>{
     // res.send('hello world')
-    res.download(path.join(__dirname,'sample.txt'))
+    res.sendFile(path.join(__dirname,'index.html'))
 })
+app.get('/download',(req,res)=>{
+    // res.send('hello world')
+    res.download(path.join(__dirname,'public/images/kitten.jpeg'))
+})
+
 
 
 app.listen(PORT,()=>{
