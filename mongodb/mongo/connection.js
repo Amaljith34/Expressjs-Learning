cbe26b476bcee8b59ec4a3490acc5083713801b2
Amaljith-aj <jith51541@gmail.com>
2024-09-18 16:50:10 +0530

    mongodb

--- mongodb/mongo/connection.js
+++ mongodb/mongo/connection.js
@@ -1,12 +1,12 @@
-// const express=require('express');
-// const app=express();
+const express=require('express');
+const app=express();
 
-// app.get('/',(req,res)=>{
-//     res.send('heloo')
+app.get('/',(req,res)=>{
+    res.send('heloo')
 
-// })
-// app.listen(3000,()=>{
-//     console.log('sucess');
+})
+app.listen(3000,()=>{
+    console.log('sucess');
     
-// })
+})
 
