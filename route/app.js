f1a10e240054fe07268e404fd8074ee47325603a
Amaljith-aj <jith51541@gmail.com>
2024-09-18 16:54:32 +0530

    route

--- route/app.js
+++ route/app.js
@@ -8,7 +8,7 @@ app.use('/product',productRouter)
 app.use('/user',userRouter)
 
 app.get('*',(req,res)=>{
-    // res.status(404)
+    res.status(404)
     //   res.send(`<h1>404</h1>`)
     res.status(404).send(`<h1>404</h1>`)
 })
